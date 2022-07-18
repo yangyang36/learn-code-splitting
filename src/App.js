22f4506d2c34c773a06bf5b5036ef68ffbb368a2
yangyang36 <9723006@nkust.edu.tw>
2022-07-18 23:26:34 +0800

    how to make own react route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg"
+import "./App.css"
+import Page1 from "./components/Page1"
+import Page2 from "./components/Page2"
+import Page3 from "./components/Page3"
+
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//      </header>
+//    </div>
+//  );
+//}
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = { route: "page1" }
+  }
+  onRouteChange = (route) => {
+    this.setState({ route })
+  }
+  render() {
+    if (this.state.route === "page1") {
+      return <Page1 onRouteChange={this.onRouteChange} />
+    } else if (this.state.route === "page2") {
+      return <Page2 onRouteChange={this.onRouteChange} />
+    } else if (this.state.route === "page3") {
+      return <Page3 onRouteChange={this.onRouteChange} />
+    }
+  }
 }
-
-export default App;
+export default App
